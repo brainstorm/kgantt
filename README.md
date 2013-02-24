a57d00fbefd1da33c36c0f3b381cb3143921e926
Roman Valls Guimera <roman.valls.guimera@scilifelab.se>
2013-02-24 21:57:25 +0100

    A bit more clarifications and TODO items in the documentation

--- README.md
+++ README.md
@@ -1,6 +1,28 @@
 kgantt
 ======
 
-An attempt to draw Gantt charts using Trello and Google APIs
+Overview:
+========
+
+An attempt to draw Gantt charts using Trello and Google charts API.
 
 ![alt text](https://raw.github.com/brainstorm/kgantt/master/gantt.png "Project management ruins lifes")
+
+At the moment this system requires:
+
+1) Manually set deadlines to all cards (tasks) that one wants to see in the gantt chart.
+2) [TODO] Manually specified dependencies via the checklist system (keyword: "Depends on" and a link or card id).
+3) That you re-run the script to update the view of the updated gantt chart (every day? every board meeting?).
+
+Installation:
+============
+
+At the moment, write two files: .key and .token
+
+They should contain the trello api developer key and token, respectively. Those can be found at:
+
+https://trello.com/1/appKey/generate#
+
+In the future that should not be necessary if one skips the OAuth step via foauth.org:
+
+https://github.com/gulopine/foauth.org/issues/18#issuecomment-14015475
--- googlegant.py
+++ googlegant.py
@@ -7,13 +7,12 @@ import sys
 
 # Trello application developer key and token
 # Available from: https://trello.com/1/appKey/generate#
-key='?key='+open(".key").read().rstrip()
-token='&token='+open(".token").read().rstrip()
+key='key='+open(".key").read().rstrip()
+token='token='+open(".token").read().rstrip()
 
-# XXX todo for loop that goes through all boards
-# and fetches the times in it
-trello_url = 'https://api.trello.com/1/members/me/boards'+key+'&'+token
-trello_card = 'https://api.trello.com/1/cards/6MFWnC2r'+key+'&'+token
+
+trello_url = 'https://api.trello.com/1/members/me/boards'+'?'+key+'&'+token
+trello_card = 'https://api.trello.com/1/cards/6MFWnC2r'+'?'+key+'&'+token
 
 r = requests.get(trello_card)
 # "2013-02-28T07:00:00.000Z"
@@ -29,10 +28,15 @@ late = GanttCategory('Late', 'c00')
 upcoming = GanttCategory('Upcoming', '00c')
 
 
-
 t1 = gc.add_task('Python task', (date[0], date[1], date[2]), (date[0], date[1]+1, date[2]), category=late)
 t2 = gc.add_task('Kraulis task', depends_on=t1, duration=3, category=upcoming)
 t3 = gc.add_task('Valentine task', depends_on=t2, duration=2, category=on_time)
 
 url = gc.get_url()
-image = gc.get_image("gantt.png")
+image = gc.get_image("test_gantt.png")
+
+
+# XXX: The card should be aware on which list is in (different than list "Done" means TODO).
+# XXX: For loop that goes through all boards and fetches the times.
+# XXX: Check checklist for each card and search for "Depends on" keyword.
+# XXX: Check with status of trello support on fopen.org to simplify all the OAuth phase.
