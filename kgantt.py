e1348011ca96ac469482c618a1d9639b267b7931
Roman Valls Guimera <roman.valls.guimera@scilifelab.se>
2013-02-24 22:59:42 +0100

    Future thoughts before creating entities/classes and such

--- kgantt.py
+++ kgantt.py
@@ -5,6 +5,7 @@ from datetime import datetime
 import requests
 import sys
 
+
 # Trello application developer key and token
 # Available from: https://trello.com/1/appKey/generate#
 key='key='+open(".key").read().rstrip()
@@ -27,8 +28,10 @@ on_time = GanttCategory('On Time', '0c0')
 late = GanttCategory('Late', 'c00')
 upcoming = GanttCategory('Upcoming', '00c')
 
-
+# Python task finishes one month later after its start date (note date[1]+1)
 t1 = gc.add_task('Python task', (date[0], date[1], date[2]), (date[0], date[1]+1, date[2]), category=late)
+
+# Specifying tasks dependencies. Duration specified in days... can be adapted to datetime deltas
 t2 = gc.add_task('Kraulis task', depends_on=t1, duration=3, category=upcoming)
 t3 = gc.add_task('Valentine task', depends_on=t2, duration=2, category=on_time)
 
@@ -36,7 +39,10 @@ url = gc.get_url()
 image = gc.get_image("test_gantt.png")
 
 
-# XXX: The card should be aware on which list is in (different than list "Done" means TODO).
 # XXX: For loop that goes through all boards and fetches the times.
+# XXX: The card should be aware on which list is in (different than list "Done" means TODO).
+#		isDone() method for cards.
 # XXX: Check checklist for each card and search for "Depends on" keyword.
-# XXX: Check with status of trello support on fopen.org to simplify all the OAuth phase.
+#	Potentially use: http://ipython.org/ipython-doc/dev/parallel/dag_dependencies.html
+# XXX: Check status of trello<->foauth.org support to bridge OAuth with HTTP Basic
+#	https://github.com/gulopine/foauth.org/issues/18#issuecomment-14015475
