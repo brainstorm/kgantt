c33add9f867ad6c742ed2d6959da3e6cd651cd8c
Roman Valls Guimera <roman.valls.guimera@scilifelab.se>
2013-02-24 21:11:09 +0100

    First proof of concept, looping through boards and dependency support missing

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ kgantt
 ======
 
 An attempt to draw Gantt charts using Trello and Google APIs
+
+<img src='https://github.com/brainstorm/kgantt/blob/master/gantt.png'/>
---
+++ googlegant.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from googlegantt import GanttChart, GanttCategory
+from datetime import datetime
+import requests
+import sys
+
+# Trello application developer key and token
+# Available from: https://trello.com/1/appKey/generate#
+key='?key='+open(".key").read().rstrip()
+token='&token='+open(".token").read().rstrip()
+
+# XXX todo for loop that goes through all boards
+# and fetches the times in it
+trello_url = 'https://api.trello.com/1/members/me/boards'+key+'&'+token
+trello_card = 'https://api.trello.com/1/cards/6MFWnC2r'+key+'&'+token
+
+r = requests.get(trello_card)
+# "2013-02-28T07:00:00.000Z"
+due_date = r.json['due'].split('T')[0]
+
+date = [int(i) for i in due_date.split('-')]
+today = datetime.now().isocalendar()
+
+gc = GanttChart('Trello Gantt chart', width=650, height=200, progress=(today[0], today[1], today[2]))
+
+on_time = GanttCategory('On Time', '0c0')
+late = GanttCategory('Late', 'c00')
+upcoming = GanttCategory('Upcoming', '00c')
+
+
+
+t1 = gc.add_task('Python task', (date[0], date[1], date[2]), (date[0], date[1]+1, date[2]), category=late)
+t2 = gc.add_task('Kraulis task', depends_on=t1, duration=3, category=upcoming)
+t3 = gc.add_task('Valentine task', depends_on=t2, duration=2, category=on_time)
+
+url = gc.get_url()
+image = gc.get_image("gantt.png")
